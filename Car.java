9f87124398754383b4e02700fd5b7ac6dde6613f
Zhou Hanruoyu <146501078+ZhouHanruoyu@users.noreply.github.com>
2023-10-17 19:53:22 +0800

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,23 @@
+public class Car{
+   private String make;
+   private float price;
+
+   public Car(){}
+    public String getMake() {
+        return make;
+    }
+    public float getPrice() {
+        return price;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+    public void setPrice(float price) {
+        this.price = price;
+    }
+    public void display(){
+        System.out.println("The car make : "+this.make);
+        System.out.println("The car price : "+ this.price+" RMB");
+    }
+}
