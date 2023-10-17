7171b133a10b1d54e2abc0233e83984a96d91fd4
Zhou Hanruoyu <146501078+ZhouHanruoyu@users.noreply.github.com>
2023-10-17 20:06:25 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String args[]){
+        System.out.println("The Car Project V1.0");
+        Scanner input=new Scanner(System.in);
+        Car car1=new Car();
+        System.out.println("Enter a car make");
+        String make = input.nextLine();
+        System.out.println("Enter a car price");
+        float price = input.nextFloat();
+
+        car1.setMake(make);
+        car1.setPrice(price);
+        car1.display();
+    }
 }
