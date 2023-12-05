eaef45a36ea6f6a7be15961cdd05d1a37ad6f9d1
Zhou Hanruoyu <146501078+ZhouHanruoyu@users.noreply.github.com>
2023-12-05 11:40:56 +0800

    Update ProductRacket.java

--- ProductRacket.java
+++ ProductRacket.java
@@ -1,15 +1,14 @@
 public class ProductRacket {
-    String brand;
-    String name;
-    String BlancePoint;
-    String RigidityOfHandle;
-    String RacketType;
-public ProductRacket(String brand,String name,String BlancePoint,String RigidityOfHandle,String RacketType){
-    this.brand=brand;
-    this.name=name;
-    this.BlancePoint=BlancePoint;
-    this.RigidityOfHandle=RigidityOfHandle;
-    this.RacketType=RacketType;
+    public  String brand;
+    public  String name;
+    public  String BalancePoint;
+    public  String RigidityOfHandle;
+    public  String RacketCategory;
+    public ProductRacket(String brand,String name,String BalancePoint,String RigidityOfHandle,String RacketCategory) {
+        this.brand = brand;
+        this.name = name;
+        this.BalancePoint = BalancePoint;
+        this.RigidityOfHandle = RigidityOfHandle;
+        this.RacketCategory = RacketCategory;
+    }
  }
-
-}
