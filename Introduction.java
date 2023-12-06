6105906ab10189099751b49582f9405fba5785bb
DiJia-007 <148959205+DiJia-007@users.noreply.github.com>
2023-12-06 17:07:24 +0800

    Update Introduction.java

--- Introduction.java
+++ Introduction.java
@@ -1,3 +1,18 @@
 public class Introduction {
+ public static final String INTRODUCTION_TEXT1 =  "Badminton is a kind of indoor sport of small ball games," ;
+    public static final String INTRODUCTION_TEXT2 = "which is made by hitting feathers and cork with a long-handled net racket across the net. Badminton is played on a rectangular court," ;
+    public static final String INTRODUCTION_TEXT3 = "with a net between the courts. Both sides use various techniques and tactics such as serving," ;
+    public static final String INTRODUCTION_TEXT4 = "hitting and moving to hit the ball back and forth on the net," ;
+    public static final String INTRODUCTION_TEXT5 = "so as not to make the ball fall into their effective area or make the other side make a mistake in hitting the ball.";
 
+
+    public static String getIntroduction(){
+       return
+
+               INTRODUCTION_TEXT1+
+                       INTRODUCTION_TEXT2+
+                       INTRODUCTION_TEXT3+
+                       INTRODUCTION_TEXT4+
+                       INTRODUCTION_TEXT5;
+   }
 }
