5344babdaa7d9f1833c5878cbfa5f2d11187894c
DiJia-007 <148959205+DiJia-007@users.noreply.github.com>
2023-12-17 15:08:04 +0800

    Update ProductRacket.java

--- ProductRacket.java
+++ ProductRacket.java
@@ -1,9 +1,14 @@
 public class ProductRacket {
+    /*
+        Declare and create objects of classes:
+    "brand"/"name"/"BalancePoint"/"RigidityOfHandle"/"RacketCategory"
+    */
     public  String brand;
     public  String name;
     public  String BalancePoint;
     public  String RigidityOfHandle;
     public  String RacketCategory;
+    //initialize the classes
     public ProductRacket(String brand,String name,String BalancePoint,String RigidityOfHandle,String RacketCategory) {
         this.brand = brand;
         this.name = name;
